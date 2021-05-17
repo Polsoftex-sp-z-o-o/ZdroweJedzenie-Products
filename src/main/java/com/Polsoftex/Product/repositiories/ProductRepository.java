57831b95f7b1f243f7fd6edb4db0a2c35698b70a
Makaa88 <maka6868@gmail.com>
2021-05-17 19:44:00 +0200

    Add get for categories

--- src/main/java/com/Polsoftex/Product/repositiories/ProductRepository.java
+++ src/main/java/com/Polsoftex/Product/repositiories/ProductRepository.java
@@ -3,11 +3,14 @@ package com.Polsoftex.Product.repositiories;
 
 import com.Polsoftex.Product.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-
+    @Query("SELECT DISTINCT p.category FROM Product p")
+    List<String> getAllCategories();
 }
